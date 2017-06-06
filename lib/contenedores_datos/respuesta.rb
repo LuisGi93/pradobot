521200548cb93b7215a3922cd3a6bff461cdeb8f
LuisGi93 <luisgguijarro9@gmail.com>
2017-06-06 19:03:58 +0200

    Actualizando test, añadiendo test sobre dudas y actualizando la clase respuesta para que se cumplan los test

--- lib/contenedores_datos/respuesta.rb
+++ lib/contenedores_datos/respuesta.rb
@@ -2,14 +2,17 @@ require_relative 'conexion_bd'
 
 
 class Respuesta
-  attr_reader :contenido, :usuario
+  attr_reader :contenido, :usuario, :duda
   def initialize contenido, usuario, duda
     @contenido=contenido
     @usuario=usuario
     @duda=duda
   end
 
+  def == (y)
+    return @contenido == y.contenido && @usuario.id_telegram == y.usuario.id_telegram && @duda==y.duda
 
+  end
 end
 
 require_relative 'usuario'
