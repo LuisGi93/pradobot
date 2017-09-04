61bf24e01f252c0d361797d241d7a647e6f306dd
LuisGi93 <luisgguijarro9@gmail.com>
2017-09-04 06:32:33 -0400

    Limpiando y mejorando test sobre dudas resueltas

--- test/test_dudas_resueltas.rb
+++ test/test_dudas_resueltas.rb
@@ -7,14 +7,24 @@ describe ListarDudasResueltas do
     Accion.establecer_bot(@stub_bot)
 
     allow(@stub_mensaje).to receive_message_chain(:usuario, :id_telegram) { 66 }
+    allow(@stub_mensaje).to receive(:tipo) { '' }
+    allow(@stub_mensaje).to receive(:datos_mensaje) { 'Resueltas' }
+    allow(@stub_mensaje).to receive(:id_chat) { 1234 }
+    allow(@stub_mensaje).to receive(:id_mensaje) { 1234 }
+    allow(@stub_mensaje).to receive(:id_callback) { 1234 }
 
-    @stub_curso = double('curso')
-    allow(@stub_curso).to receive(:nombre) { 'nombre del curso' }
-    allow(@stub_curso).to receive(:id_moodle) { 5 }
     stub_padre = double('accion_padre')
     allow(stub_padre).to receive(:cambiar_curso)
     allow(stub_padre).to receive(:cambiar_curso_parientes)
     allow(stub_padre).to receive(:curso) { @stub_curso }
+
+    @stub_curso = double('curso')
+    allow(@stub_curso).to receive(:nombre) { 'nombre del curso' }
+    allow(@stub_curso).to receive(:id_moodle) { 5 }
+    allow(@stub_curso).to receive(:obtener_profesor_curso) { @stub_usuario1 }
+    allow(@stub_curso).to receive(:obtener_dudas_resueltas) { array_dudas_curso }
+
+
     @accion = MenuDudas.new(stub_padre)
     @accion.cambiar_curso(@stub_curso)
 
@@ -28,67 +38,86 @@ describe ListarDudasResueltas do
     allow(@stub_usuario1).to receive(:nombre_usuario) { 'usuario1' }
     allow(@stub_usuario2).to receive(:nombre_usuario) { 'usuario2' }
     allow(@stub_usuario3).to receive(:nombre_usuario) { 'usuario3' }
-  end
 
-  it 'Debe poder mostrar dudas resueltas curso' do
-    allow(@stub_mensaje).to receive(:tipo) { '' }
-    allow(@stub_mensaje).to receive(:datos_mensaje) { 'Resueltas' }
+
+
+    array_dudas_curso = []
+    @stub_duda = double('duda2')
+    allow(@stub_duda).to receive(:contenido) { 'contenido duda 2' }
+    allow(@stub_duda).to receive(:usuario) { @stub_usuario2 }
+    array_dudas_curso << Duda.new('contenido duda 1', @stub_usuario1)
+    @stub_respuesta = double('respuesta')
+    allow(@stub_respuesta).to receive(:contenido) { 'Contenido de la solución de la duda' }
+    allow(@stub_duda).to receive(:solucion) { @stub_respuesta }
+    array_dudas_curso << @stub_duda
+    allow(@stub_curso).to receive(:obtener_dudas_resueltas) { array_dudas_curso }
+
+
+
 
     array_dudas_curso = []
     array_dudas_curso << Duda.new('contenido duda 1', @stub_usuario1)
-    array_dudas_curso << Duda.new('contenido duda 2', @stub_usuario2)
+    array_dudas_curso << @stub_duda
     array_dudas_curso << Duda.new('contenido duda 3', @stub_usuario3)
+    allow(@stub_curso).to receive(:obtener_dudas_resueltas) { array_dudas_curso }
 
-    expect(@stub_curso).to receive(:obtener_dudas_resueltas) { array_dudas_curso }
+    sut=double('sut')
+    allow(sut).to receive_message_chain(:[], :[])
+    allow(@stub_bot).to receive_message_chain(:api, :send_message).and_return(sut)
+    allow(@stub_bot).to receive_message_chain(:api, :edit_message_text)
+    allow(@stub_bot).to receive_message_chain(:api, :answer_callback_query) { 1234 }
+
+
+
+
+  end
+
+  it 'Debe poder mostrar dudas resueltas curso' do
+    double_id_ultimo_mensaje=double('double_aux')
+    allow(double_id_ultimo_mensaje).to receive_message_chain(:[], :[])
+
+    expect(@stub_bot).to receive_message_chain(:api, :send_message){|arg1|
+        expect(arg1[:text]).to include('Dudas resueltas para nombre del curso', 'contenido duda 1', 'contenido duda 2', 'contenido duda 3')
+      }.and_return(double_id_ultimo_mensaje)
+
+    @accion.recibir_mensaje(@stub_mensaje)
+  end
+
+  it 'Las dudas resueltas deben ser extraidas del curso activo' do
+    expect(@stub_curso).to receive(:obtener_dudas_resueltas)
     expect(@stub_bot).to receive_message_chain(:api, :send_message, :[], :[])
 
     @accion.recibir_mensaje(@stub_mensaje)
   end
 
-  it 'Debe poder ver las acciones que se pueden hacer sobre una duda resuelta' do
-    allow(@stub_mensaje).to receive(:tipo) { '' }
-    allow(@stub_mensaje).to receive(:datos_mensaje) { 'Resueltas' }
-    allow(@stub_mensaje).to receive(:id_chat) { 1234 }
-    allow(@stub_mensaje).to receive(:id_mensaje) { 1234 }
-    array_dudas_curso = []
+  it 'Al mostrar las dudas resueltas debe incluir un teclado para selecciona una duda.' do
+    double_id_ultimo_mensaje=double('return')
+    allow(double_id_ultimo_mensaje).to receive_message_chain(:[], :[])
 
-    array_dudas_curso << Duda.new('contenido duda 1', @stub_usuario1)
-    array_dudas_curso << Duda.new('contenido duda 2', @stub_usuario2)
-    array_dudas_curso << Duda.new('contenido duda 3', @stub_usuario3)
-    allow(@stub_curso).to receive(:obtener_dudas_resueltas) { array_dudas_curso }
-    allow(@stub_bot).to receive_message_chain(:api, :send_message, :[], :[])
+     expect(@stub_bot).to receive_message_chain(:api, :send_message){|arg1|
+      expect(arg1.keys).to include(:reply_markup)
+    }.and_return(double_id_ultimo_mensaje)
+
+     @accion.recibir_mensaje(@stub_mensaje)
+  end
+
+  it 'Debe poder ver las acciones que se pueden hacer sobre una duda resuelta' do
+
     @accion.recibir_mensaje(@stub_mensaje)
 
     allow(@stub_mensaje).to receive(:tipo) { 'callbackquery' }
     allow(@stub_mensaje).to receive(:datos_mensaje) { '##$$duda_1' }
 
-    allow(@stub_curso).to receive(:obtener_profesor_curso) { @stub_usuario1 }
 
     expect(@stub_bot).to receive_message_chain(:api, :edit_message_text) { |arg1|
-                           arg1.keys.should include(:reply_markup)
+                           expect(arg1.keys).to include(:reply_markup)
                            expect(arg1[:text]).to include('contenido duda 2', 'elegida')
                          }
-    allow(@stub_bot).to receive_message_chain(:api, :edit_message_text)
-
     @accion.recibir_mensaje(@stub_mensaje)
   end
 
   it 'En las acciones sobre una duda resuelta debe tenerse en cuenta si el usuario es el creador de la duda o el profesor del curso' do
-    allow(@stub_mensaje).to receive(:tipo) { '' }
-    allow(@stub_mensaje).to receive(:datos_mensaje) { 'Resueltas' }
-    allow(@stub_mensaje).to receive(:id_chat) { 1234 }
-    allow(@stub_mensaje).to receive(:id_mensaje) { 1234 }
-    array_dudas_curso = []
-    stub_duda = double('duda2')
-    allow(stub_duda).to receive(:contenido) { 'contenido duda 2' }
-    allow(stub_duda).to receive(:usuario) { @stub_usuario2 }
-    array_dudas_curso << Duda.new('contenido duda 1', @stub_usuario1)
-    stub_respuesta = double('respuesta')
-    allow(stub_respuesta).to receive(:contenido) { 'Contenido de la solución de la duda' }
-    allow(stub_duda).to receive(:solucion) { stub_respuesta }
-    array_dudas_curso << stub_duda
-    allow(@stub_curso).to receive(:obtener_dudas_resueltas) { array_dudas_curso }
-    allow(@stub_bot).to receive_message_chain(:api, :send_message, :[], :[])
+
 
     @accion.recibir_mensaje(@stub_mensaje)
 
@@ -96,48 +125,23 @@ describe ListarDudasResueltas do
     allow(@stub_mensaje).to receive(:datos_mensaje) { '##$$duda_1' }
     allow(@stub_bot).to receive_message_chain(:api, :edit_message_text)
 
-    expect(stub_duda).to receive(:usuario) { @stub_usuario2 }
+    expect(@stub_duda).to receive_message_chain(:usuario,:id_telegram)
     expect(@stub_curso).to receive(:obtener_profesor_curso) { @stub_usuario1 }
 
     @accion.recibir_mensaje(@stub_mensaje)
   end
   it 'Debe poderse ver la solución de una duda resuelta' do
-    allow(@stub_mensaje).to receive(:tipo) { '' }
-    allow(@stub_mensaje).to receive(:datos_mensaje) { 'Resueltas' }
-    allow(@stub_mensaje).to receive(:id_chat) { 1234 }
-    allow(@stub_mensaje).to receive(:id_mensaje) { 1234 }
-    allow(@stub_mensaje).to receive(:id_callback) { 1234 }
-    allow(@stub_bot).to receive_message_chain(:api, :answer_callback_query) { 1234 }
-
-    array_dudas_curso = []
-    stub_duda = double('duda2')
-    allow(stub_duda).to receive(:contenido) { 'contenido duda 2' }
-    allow(stub_duda).to receive(:usuario) { @stub_usuario2 }
-    array_dudas_curso << Duda.new('contenido duda 1', @stub_usuario1)
-    stub_respuesta = double('respuesta')
-    allow(stub_respuesta).to receive(:contenido) { 'Contenido de la solución de la duda' }
-    allow(stub_duda).to receive(:solucion) { stub_respuesta }
-    array_dudas_curso << stub_duda
-    array_dudas_curso << Duda.new('contenido duda 3', @stub_usuario3)
-    allow(@stub_curso).to receive(:obtener_dudas_resueltas) { array_dudas_curso }
-    allow(@stub_bot).to receive_message_chain(:api, :send_message, :[], :[])
-
     @accion.recibir_mensaje(@stub_mensaje)
 
     allow(@stub_mensaje).to receive(:tipo) { 'callbackquery' }
     allow(@stub_mensaje).to receive(:datos_mensaje) { '##$$duda_1' }
-    allow(@stub_curso).to receive(:obtener_profesor_curso) { @stub_usuario1 }
-    allow(@stub_bot).to receive_message_chain(:api, :edit_message_text)
 
     @accion.recibir_mensaje(@stub_mensaje)
 
     allow(@stub_mensaje).to receive(:datos_mensaje) { '##$$Solución duda' }
-    stub_respuesta = double('respuesta')
-    allow(stub_respuesta).to receive(:contenido) { 'Contenido de la solución de la duda' }
-    allow(stub_duda).to receive(:solucion) { stub_respuesta }
 
-    expect(stub_duda).to receive(:solucion)
-    expect(stub_respuesta).to receive(:contenido)
+    expect(@stub_duda).to receive(:solucion)
+    expect(@stub_respuesta).to receive(:contenido)
     expect(@stub_bot).to receive_message_chain(:api, :edit_message_text) { |arg1|
       expect(arg1[:text]).to include('Contenido de la solución de la duda', 'contenido duda 2')
     }
