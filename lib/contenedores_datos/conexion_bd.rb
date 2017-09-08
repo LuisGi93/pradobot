0c0f2886f5c942c51a5eba9503722fcf86687195
LuisGi93 <luisgguijarro9@gmail.com>
2017-09-08 08:47:03 -0400

    Añadiendo comprobación de la entrada del usuario cuando introduce texto, verificando email válido al inicializar usuario

--- lib/contenedores_datos/conexion_bd.rb
+++ lib/contenedores_datos/conexion_bd.rb
@@ -3,4 +3,18 @@ require 'sequel'
 #Clase de la que heredan todas aquellas que quieran utilizar la base de datos de la aplicación 
 class ConexionBD
   @@db = Sequel.connect(ENV['URL_DATABASE_TRAVIS'])
+
+  def verificar_entrada_texto string
+    if string.contains('id_moodle', 'nombre_curso', 'id_chat', 'nombre_chat', 'chat_curso', 'nombre_chat_telegram',
+                      'id_moodle_curso', 'nombre_chat_telegram', 'usuario_telegram', 'nombre_usuario' , 'id_telegram',
+                       'usuario_telegram', 'estudiante_curso', 'id_moodle_curso', 'usuarios_moodle', 'token',
+                       'id_profesor', 'dia_semana_hora', 'estado', 'hora_solicitud', 'id_usuario_duda', 'contenido_duda',
+                       'id_usuario_respuesta', 'contenido_respuesta', 'respuesta_resuelve_duda'
+          )
+      return false
+    else
+      return true
+    end
+
+  end
 end
