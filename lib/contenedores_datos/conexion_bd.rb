69d1f4ee5e4078f9f585556c266df9217d2fef75
LuisGi93 <luisgguijarro9@gmail.com>
2017-09-09 06:04:00 -0400

    Arreglando detección palabras claves clases en las que hacne uso de las entradas de los usuarios para prevenir sql injection

--- lib/contenedores_datos/conexion_bd.rb
+++ lib/contenedores_datos/conexion_bd.rb
@@ -5,12 +5,13 @@ class ConexionBD
   @@db = Sequel.connect(ENV['URL_DATABASE_TRAVIS'])
 
   def verificar_entrada_texto string
-    if string.contains('id_moodle', 'nombre_curso', 'id_chat', 'nombre_chat', 'chat_curso', 'nombre_chat_telegram',
-                      'id_moodle_curso', 'nombre_chat_telegram', 'usuario_telegram', 'nombre_usuario' , 'id_telegram',
-                       'usuario_telegram', 'estudiante_curso', 'id_moodle_curso', 'usuarios_moodle', 'token',
-                       'id_profesor', 'dia_semana_hora', 'estado', 'hora_solicitud', 'id_usuario_duda', 'contenido_duda',
-                       'id_usuario_respuesta', 'contenido_respuesta', 'respuesta_resuelve_duda'
-          )
+
+    palabras_clave=['id_moodle', 'nombre_curso', 'id_chat', 'nombre_chat', 'chat_curso', 'nombre_chat_telegram',
+                   'id_moodle_curso', 'nombre_chat_telegram', 'usuario_telegram', 'nombre_usuario' , 'id_telegram',
+                   'usuario_telegram', 'estudiante_curso', 'id_moodle_curso', 'usuarios_moodle', 'token',
+                   'id_profesor', 'dia_semana_hora', 'estado', 'hora_solicitud', 'id_usuario_duda', 'contenido_duda',
+                   'id_usuario_respuesta', 'contenido_respuesta', 'respuesta_resuelve_duda']
+    if palabras_clave.include?(string)
       return false
     else
       return true
